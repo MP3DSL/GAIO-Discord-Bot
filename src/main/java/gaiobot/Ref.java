1d6936979c5c8dc2354b60d06fe3c2af28f2c76a
Derrick Lemmon <dsl1130@gmail.com>
2020-03-12 14:45:15 -0400

    Removed token for public viewing

--- src/main/java/gaiobot/Ref.java
+++ src/main/java/gaiobot/Ref.java
@@ -5,7 +5,7 @@ public class Ref{
      * 
      */
 	/*Bot Settings*/
-    public static String token = "NTQyMTIzOTQ2MTgzNjIyNjg2.XmphcQ.7oaDuX4l72oWCXgGi_-vLwyLifM";
+    public static String token = "";
     public static String prefix = ">";
     public static String version = "0.2";
     public static final int defPower = 0;
